b58336bff9fd42a1c93a2c2c1f53801220943007
vicissic <victor.velesco@gmail.com>
2025-10-09 18:16:33 +0200

    making easy read and write function

---
+++ exercises/first_program.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline)
+void copy(char to_array[], char from_array[])
+
+main()
+{
+    int cur_max, len;
+    char cur_line[MAXLINE];
+    char longest_line[MAXLINE];
+
+    max = 0
+    while ((len = getline(cur_line)) != 0){
+        if (len > cur_max){
+            cur_max = len;
+            copy(longest_line,cur_line);
+        }
+    }
+    if (max > 0){
+        printf("%s", longest);
+    return 0;
+    }
+
+
+}
+
